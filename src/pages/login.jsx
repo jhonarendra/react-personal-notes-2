0df8aa37c3e2e78e035dda64fd11128bfafdc388
jhonarendra <jhonarendra@gmail.com>
2022-09-24 08:54:24 +0800

    feat: login n register page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function LoginPage() {
   return (
-    <div>
-      Login
-    </div>
+    <section className="login-page">
+      <h2>Yuk, login untuk menggunakan aplikasi.</h2>
+      <div className="input-login">
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" value="" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" value="" />
+        <button type="button">Login</button>
+      </div>
+      <p className="login-page__footer">
+        Belum punya akun?
+        {' '}
+        <Link to="/register">Daftar di sini</Link>
+      </p>
+    </section>
   )
 }
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function RegisterPage() {
   return (
-    <div>
-      Register
-    </div>
+    <section className="register-page">
+      <h2>Isi form untuk mendaftar akun.</h2>
+      <div className="input-register">
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" value="" />
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" value="" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" value="" />
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input type="password" id="confirmPassword" value="" />
+        <button type="button">Register</button>
+      </div>
+      <p className="register-page__footer">
+        Sudah punya akun?
+        {' '}
+        <Link to="/login">Login di sini</Link>
+      </p>
+    </section>
   )
 }
