3a4b2234175a2f1e04899834e71a8ebc03e2c285
jhonarendra <jhonarendra@gmail.com>
2022-09-24 15:06:35 +0800

    fix: logout button only in auth

--- src/components/layout/LogoutButton.jsx
+++ src/components/layout/LogoutButton.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { MdLogout } from 'react-icons/md'
+import LocaleContext from '../../contexts/LocaleContext'
 
 export default function LogoutButton() {
+  const { auth } = useContext(LocaleContext)
+
   const handleLogout = () => {
     if (confirm('Yakin ingin keluar?')) {
       alert('berhasil')
     }
   }
   return (
-    <button
-      type="button"
-      title="Logout"
-      className="button-logout"
-      onClick={handleLogout}
-    >
-      <MdLogout />
-    </button>
+    <>
+    {
+      auth ? (
+        <button
+          type="button"
+          title="Logout"
+          className="button-logout"
+          onClick={handleLogout}
+        >
+          <MdLogout />
+        </button>
+      ) : ''
+    }
+    </>
   )
 }
